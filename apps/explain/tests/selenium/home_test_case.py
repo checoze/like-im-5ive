8e0861d846669cd428014480f1d447fcc2e77ba2
Derek Stegelman <dstegelman@gmail.com>
2012-08-17 22:56:56 -0500

    Add travis, and first test case.

--- apps/explain/tests/selenium/home_test_case.py
+++ apps/explain/tests/selenium/home_test_case.py
@@ -1,2 +1,14 @@
-__author__ = 'Derek Stegelman'
-__date__ = '8/17/12'
+from django.test import LiveServerTestCase
+from selenium import webdriver
+
+class HomePageTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        self.brower.get(self.live_server_url)
