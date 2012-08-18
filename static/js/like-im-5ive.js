29055895ee79cb8ff72a1aa319bf6852f1421657
Jon Faustman <faustman@k-state.edu>
2012-08-18 13:27:25 -0500

    Add GA code.

--- static/js/like-im-5ive.js
+++ static/js/like-im-5ive.js
@@ -3,3 +3,13 @@ $(function(){
         e.stopPropagation();
     });
 });
+// Google Analtyics
+/*
+var _gaq = [['_setAccount', 'UA-34096189-1'], ['_trackPageview']];
+(function(d, t) {
+  var g = d.createElement(t),
+      s = d.getElementsByTagName(t)[0];
+  g.src = '//www.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g, s);
+}(document, 'script'));
+*/
