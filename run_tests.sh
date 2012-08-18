de7c2082e65fa31c9d752eee4d4d8fa246bf0589
Derek Stegelman <dstegelman@gmail.com>
2012-08-17 23:21:02 -0500

    Use local settings for tests.

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ echo "** Running Unit Tests **"
 
 echo "** Explain **"
 coverage -e
-coverage -x manage.py test explain --settings=settings.test
+coverage -x manage.py test explain --settings=settings.local
 
 echo "** WRITING COVERAGE **"
 coverage html -d ./reports/coverage_html
