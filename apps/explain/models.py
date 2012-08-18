1fdc20a3ca05fb72072b211bd460ac09fcbfe5f5
Garrett Pennington <garrettp@gmail.com>
2012-08-17 21:47:16 -0500

    model stubbing

--- apps/explain/models.py
+++ apps/explain/models.py
@@ -1,1 +1,20 @@
-#models
+from django.db import models
+from django.contrib.auth.models import User
+
+class Entry(models.Model):
+    """ Describes an Entry"""
+    #name
+    #slug
+    #type
+
+    #created_date = models.DateTimeField(auto_add_now=True)
+    #deleted_date = models.DateTimeField()
+    #deleted = models.BooleanField(default=False)
+    
+    #original_creator
+
+    #submitted
+        
+    def __unicode__(self):
+        return str(self.slug)
+
