8cde44930a38c1b7eb494702f1b509640ba819d3
Garrett Pennington <garrettp@gmail.com>
2012-08-18 10:32:58 -0500

    explanation inline formset

--- apps/explain/forms.py
+++ apps/explain/forms.py
@@ -1,8 +1,22 @@
 from django.forms import ModelForm
-from explain.models import Entry
+from django.forms.models import inlineformset_factory
+
+from explain.models import Entry, Explanation
 
 class EntryForm(ModelForm):
     
     class Meta:
         model = Entry
         exclude = ('deleted_date', 'deleted', 'hex', 'slug',)
+        
+        
+
+class ExplanationForm(ModelForm):
+    
+    class Meta:
+        model = Explanation
+        exclude = ('deleted_date', 'deleted', )
+        
+        
+
+ExplanationFormset = inlineformset_factory(Entry, Explanation, max_num=1, can_delete=False, form=ExplanationForm)
