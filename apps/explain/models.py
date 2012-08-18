bc0f7a9b1201e5e28e6b7b5d25f55754a2a94543
Garrett Pennington <garrettp@gmail.com>
2012-08-17 22:16:02 -0500

    installed apps

--- apps/explain/models.py
+++ apps/explain/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Base(models.Model):
     """ Base model that contains creation data """
 
-    created_date = models.DateTimeField(auto_add_now=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     deleted_date = models.DateTimeField()
     deleted = models.BooleanField(default=False)
 
@@ -16,7 +16,7 @@ class Entry(Base):
     slug = models.SlugField(max_length=255)
     type  = models.CharField(max_length=40)
     
-    original_creator = model.ForeignKey(Entry)
+    original_creator = models.ForeignKey(User)
         
     def __unicode__(self):
         return str(self.slug)
@@ -24,7 +24,7 @@ class Entry(Base):
 class Explanation(Base):
     """ An Explanation that points to an Entry """
     
-    entry = model.ForeignKey(Entry)
+    entry = models.ForeignKey(Entry)
     body = models.TextField()
 
     #upvotes
@@ -38,7 +38,7 @@ class Explanation(Base):
 class Comment(Base):
     """ Comments on an Explanation. """
     
-    user = model.ForeignKey(User)
+    user = models.ForeignKey(User)
     body = models.TextField()
     
     def __unicode__(self):
