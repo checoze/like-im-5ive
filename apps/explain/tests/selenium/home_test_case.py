f66fa6209f131afc0b26b547cc29f03700abc151
Derek Stegelman <dstegelman@gmail.com>
2012-08-17 23:20:32 -0500

    Test case update

--- apps/explain/tests/selenium/home_test_case.py
+++ apps/explain/tests/selenium/home_test_case.py
@@ -6,9 +6,8 @@ class HomePageTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-
     def tearDown(self):
         self.browser.quit()
 
     def test_home_page(self):
-        self.brower.get(self.live_server_url)
+        self.browser.get(self.live_server_url)
