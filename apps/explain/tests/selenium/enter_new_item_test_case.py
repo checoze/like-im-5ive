16aed9574a02d2dc8e0c0b6b4637c1173d728443
Derek Stegelman <dstegelman@gmail.com>
2012-08-18 10:47:37 -0500

    Test update

--- apps/explain/tests/selenium/enter_new_item_test_case.py
+++ apps/explain/tests/selenium/enter_new_item_test_case.py
@@ -15,4 +15,5 @@ class NewItemTestCase(LiveServerTestCase):
         self.browser.find_element_by_id("search-button").click()
         self.browser.find_element_by_id("id_name").send_keys("reddit")
         self.browser.find_element_by_id("id_type").send_keys("http://www.reddit.com/")
+        self.browser.find_element_by_id("id_explanation_set-0-body").send_keys("Reddit is a fun place to share links.")
         self.browser.find_element_by_class_name("btn").click()
