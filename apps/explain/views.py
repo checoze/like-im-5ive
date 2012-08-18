9ee466471574b641cac291b5efad925d66afa45e
Garrett Pennington <garrettp@gmail.com>
2012-08-17 22:52:23 -0500

    custom entry save method make random hex code

--- apps/explain/views.py
+++ apps/explain/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.conf import settings
 from django.views.generic import DetailView, ListView
 
+from explain.models import Entry
 
 def home(request):
     """ Simple homepage invites users to search for or create an entry """
     context = {}
+    print request.POST
+    
+    #term, created = Entry.objects.get_or_create()
+    
 
     return render(request, 'explain/home.html', context)
