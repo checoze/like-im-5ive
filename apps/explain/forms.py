3b15360737992de138356ca7f51977487a07305a
Derek Stegelman <dstegelman@gmail.com>
2012-08-18 13:14:27 -0500

    Account registration.

--- apps/explain/forms.py
+++ apps/explain/forms.py
@@ -1,9 +1,9 @@
-from django.forms import ModelForm
+from django import forms
 from django.forms.models import inlineformset_factory
 
 from explain.models import Entry, Explanation
 
-class EntryForm(ModelForm):
+class EntryForm(forms.ModelForm):
     
     class Meta:
         model = Entry
@@ -11,12 +11,14 @@ class EntryForm(ModelForm):
         
         
 
-class ExplanationForm(ModelForm):
+class ExplanationForm(forms.ModelForm):
     
     class Meta:
         model = Explanation
         exclude = ('deleted_date', 'deleted', )
         
-        
+class RegistrationForm(forms.Form):
+    username = forms.CharField()
+    password = forms.PasswordField()
 
 ExplanationFormset = inlineformset_factory(Entry, Explanation, max_num=1, can_delete=False, form=ExplanationForm)
