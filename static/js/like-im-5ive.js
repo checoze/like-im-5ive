37f747c931a158ab7f3cc8bd194dc9dfa8aa75e9
Garrett Pennington <garrettp@gmail.com>
2012-08-18 19:55:25 -0500

    moved js to project level js

--- static/js/like-im-5ive.js
+++ static/js/like-im-5ive.js
@@ -1,8 +1,44 @@
+/* ===========================================================
+ * Like I'm 5ive
+ * http://www.likeim5ive.com
+ * ===========================================================
+ * 
+ * Author: Garrett Pennington
+ * ========================================================== */
+
 $(function(){
+    
+    function vote(elem, direction){
+		$.ajax({
+			type: "POST",
+			url: "/vote/" + $(elem).attr('data-type') + "/" + $(elem).attr('data-id') + "/",
+			dataType: 'json',
+			data: "csrfmiddlewaretoken=" + csrf_token + "&direction=" + direction,
+			success: function(data){
+			    alert('success!');
+			},
+			error: function(){
+			    console.log('Unable to vote')
+            }
+	    });
+    }
+
+    //Vote
+    $('.lif-upvote').click(function(event){
+        event.preventDefault();
+        vote( $(this), 'up' );
+    });
+    $('.lif-downvote').click(function(event){
+        event.preventDefault();
+        vote( $(this), 'down' );
+    });
+    
+    
     $('.dropdown-menu').click(function(e){
         e.stopPropagation();
     });
 });
+
 // Google Analtyics
 /*
 var _gaq = [['_setAccount', 'UA-34096189-1'], ['_trackPageview']];
