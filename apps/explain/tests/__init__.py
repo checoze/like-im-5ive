87fd67378fb0a57632c418dcf9cace0709c17237
Derek Stegelman <dstegelman@gmail.com>
2012-08-19 10:38:40 -0500

    debug travis.

--- apps/explain/tests/__init__.py
+++ apps/explain/tests/__init__.py
@@ -2,7 +2,7 @@ from selenium.home_test_case import *
 from selenium.search_homepage_test_case import *
 from selenium.enter_new_item_test_case import *
 from selenium.test_registration import *
-#from selenium.test_login import *
+from selenium.test_login import *
 from selenium.test_add_explanation import *
 #from selenium.test_up_vote import *
 #from selenium.test_down_vote import *
